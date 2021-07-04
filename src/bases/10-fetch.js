1d8112a6ef93922c5e9a9f0c7138c316d637b293
gerardoluisbv <gerardoluisbv@gmail.com>
2021-07-04 14:06:23 -0500

    Uso de fecth para las peticiones

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,18 @@
+
+const apiKey = 'Hn15UQLWUTekfQCtI7MLm89zxqzIPu5h';
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+
+peticion
+    .then( resp => resp.json() )
+    .then( ({ data }) => {
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+    
+    }) 
+.catch ( console.warn );
+
